f43fab858d8203d3a3c0f17430a908be852a7807
dyh <dyh858@163.com>
2021-04-03 16:48:06 +0800

    dz20210403

---
+++ LzglMVC/Controllers/PostController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Models;
+using BLL;
+using System.Web.Script.Serialization;
+
+namespace LzglMVC.Controllers
+{
+    public class PostController : Controller
+    {
+        //
+        // GET: /Post/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetPostList()
+        {
+            return Json(new JavaScriptSerializer().Serialize(new PostManager().list()),JsonRequestBehavior.AllowGet);
+        }
+        public String GetPostTable()
+        {
+            return new PostManager().getTable();
+        }
+
+    }
+}
---
+++ LzglMVC/Scripts/Post/index_post.js
@@ -0,0 +1,76 @@
+﻿$(function () {
+    var vm=new Vue({
+        el: "#post",
+        data: {
+            lists: [],
+            checkAll: false,//全选checkAll
+        },
+        methods: {
+            getLists: function () {
+                var self = this;
+                //axios({
+                //    method: "get",
+                //    url:"/Post/GetPostList"
+                //}).then(function (res) {
+                //    this.lists = JSON.parse(res);
+                //    console.log(res);
+                //}).catch(function (err) {
+                //    console.log(err);
+                //})
+                $.getJSON("/Post/GetPostTable", {}, function (response) {
+                    self.lists = response;
+                });
+            },
+            checkALL: function () {
+                var bool = document.getElementById("all").checked;
+                var items = document.getElementsByName("check");
+                items.forEach(item=>item.checked=bool);
+            },
+            checkItem:function(){
+                var allItem=document.getElementById("all");
+                var items=document.getElementsByName("check");
+                var countAll=items.length;
+
+                var countChecked=function(){
+                    var count=0;
+                    items.forEach(function(item,index){
+                        if(item.checked){
+                            count++;
+                        }
+                    });
+                    return count;
+                }();
+                if(countChecked==countAll){
+                    allItem.checked=true;
+                }else{
+                    allItem.checked=false;
+                }
+            },
+        },
+        mounted: function () {
+            this.getLists();
+        }
+
+    });
+    var parent = document.getElementById("dept");
+    var ele = document.getElementById("drag");
+    ele.onmousedown = function (e) {
+        disX = e.clientX - ele.offsetLeft;
+        document.onmousemove = function (ev) {
+            $('#dept').css('flex-basis',ev.clientX - disX + ele.offsetWidth);
+        }
+        document.onmouseup = function () {
+            document.onmousemove = document.onmouseup = null;
+        }
+    }
+
+})
+//$(function () {
+//    $.getJSON("/Post/GetPostList", {}, function (response) {
+//        console.log(response);
+
+//    });
+
+
+
+//})
